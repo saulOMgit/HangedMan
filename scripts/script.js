b6b9006467df882c15b277e02f79f82ed3081382
saulOMgit <sauloteromelchor@gmail.com>
2023-02-04 20:17:39 +0100

    Proyecto Terminado

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,9 @@ window.onload = function(){
     const ctx = document.querySelector("canvas").getContext("2d");
 
     //Creamos un array fijo para el juego
-    const palabras= ["Lucha","Libertad","Titan","Mtusa","Albersou","Espada","Violeta","Zombie","Java","Lugia","Hueco","Bosque","Demonio","Merengue","Antonio","Caraculo","Matrix","Korea","Japon","Gorrion"];
+    const palabras= ["Lucha","Libertad","Titan","Mtusa","Albersou","Espada","Violeta","Zombie","Java","Lugia","Hueco","Bosque","Demonio","Merengue","Antonio","Baloncesto","Matrix","Korea","Japon","Gorrion"];
     
-    const abcdario = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    //const abcdario = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     
     //Generamos un numero aleatorio que escogera automaticamente una palabra
     let numAleat= GeneraAleatorio();
@@ -17,8 +17,10 @@ window.onload = function(){
 
     console.log(numAleat);
 
+    //Esa palabra la convertimos en array de letras mayusculas para operar sobre ella
     let palabra=palabras[numAleat].toUpperCase().split("");
 
+    //Creamos un span por cada letra de la palabra, como no son de la misma longitud variara segun se escoja una u otra
     for (letra of palabra){
         let mispan=document.createElement("span");
 
@@ -30,7 +32,7 @@ window.onload = function(){
    
     //Añadimos una funcion al body para cada vez que pulsemos una tecla
     document.body.addEventListener("keydown",juego);
-    //Y codificamos la funcion     
+    //necesitaremos operar sobre los spans creados tanto para el evento de teclado como para la generación de pista    
     let spans=document.querySelectorAll(".letra");
     
     //Lanzamos la funcion que generara la pista
@@ -53,24 +55,20 @@ window.onload = function(){
     }
 
     function juego(even){
-        let mip=document.createElement("p");
+        let contador=0;
+        let existe=false;
         
-          let contador=0;
-          let existe=false;
-        
-            let mayus=even.key.toUpperCase();
+        let mayus=even.key.toUpperCase();
             //para cada letra de nuestra palabra comprobamos si coincide con la pulsada
-            for (letra of palabra){
-                if (mayus==letra){
-                    //si coincide, pintamos la letra donde corresponda (con el contador)
-                    spans[contador].textContent=mayus;
-                    mip.textContent=`${even.code}`;
-            document.body.appendChild(mip);
-                    //y marcamos existe como true, para que no cuente como un intento fallido
-                    existe=true;
-                }
-                contador++;
+        for (letra of palabra){
+            if (mayus==letra){
+                //si coincide, pintamos la letra donde corresponda (con el contador)
+                spans[contador].textContent=mayus;
+                //y marcamos existe como true, para que no cuente como un intento fallido
+                existe=true;
             }
+            contador++;
+        }
 
         //Si no existe, llamamos a la funcion fallo
         if(!existe){
@@ -107,6 +105,8 @@ window.onload = function(){
             case 6:
                 pintaCuerpo();
                 break;
+            //Al septimo fallo se nos notificara que hemos perdido, a parte de darnos la oportunidad de volver a jugar
+            // y dejaremos de poder escribir    
             case 7:
                 pintaExtremidades();
                 let perdiste=document.createElement("p");
@@ -129,15 +129,17 @@ window.onload = function(){
 
     }
 
+    //Cada vez que hagamos uso del keydown se comprobara si la palabra esta completa o no
     function correccion(){
-
+        //Para ello guardaremos las letras que se encuentran en ese momento en los span y se comprobara
+        //  si es igual a la palabra que se ha designado
         let mispan=document.querySelectorAll(".letra");
         let mipalabra="";
         for (span of mispan){
             mipalabra+=span.textContent;
         }
         console.log(mipalabra);
-
+        //Se comprueba, se notifica la victoria y se da la oportunidad de volver a jugar
         if(palabras[numAleat].toUpperCase()==mipalabra){
             document.body.removeEventListener("keydown",juego);
 
@@ -153,11 +155,13 @@ window.onload = function(){
             document.body.appendChild(spreiniciar);
         }
     }
-
+    //Funcionalidad que recarga la pagina (Creditos a Carlota)
     function reiniciar(){
         window.location.reload();
     }
-
+    //Esta funcion sera parecida a la de jugar, con la diferencia de que guardara las letras
+    // que ya hemos utilizado y sin repetirlas, nos pintara en la pagina cuales son para que no
+    // las utilicemos de nuevo
     function pintaUsadas(even){
         let usada=document.querySelector(".usada");
         let usadas=document.querySelectorAll(".usadas");
@@ -182,7 +186,7 @@ window.onload = function(){
         }
     }
     }
-
+    //De aqui hasta el final estan definidas las partes del dibujo segmentadas para utilizar con cada intento fallido
     function pintaBase(){
         //base
        ctx.beginPath();
